a18c4f1d357c342aced684ce6d1abd941559ec4b
Tony Wong <wongtonyb@gmail.com>
2019-09-04 16:58:48 -0400

    mergeSort

--- mergesort.js
+++ mergesort.js
@@ -1,40 +1,36 @@
 function split(wholeArray) {
-    let half = Math.ceil(wholeArray.length / 2);
+  let half = Math.ceil(wholeArray.length / 2);
 
-    let firstHalf = wholeArray.slice(0, half);
-    let secondHalf = wholeArray.slice(half);
+  let firstHalf = wholeArray.slice(0, half);
+  let secondHalf = wholeArray.slice(half);
 
-    return [firstHalf, secondHalf];
+  return [firstHalf, secondHalf];
 }
 
 function merge(firstArr, secondArr) {
-    let sortedArr = [];
-    let length = firstArr.length + secondArr.length;
+  let sortedArr = [];
+  let length = firstArr.length + secondArr.length;
 
-    for (let i = 0; i < length; i++) {
-     
-        let firstNum = firstArr[0];
-        let secondNum = secondArr[0];
+  for (let i = 0; i < length; i++) {
+    let firstNum = firstArr[0];
+    let secondNum = secondArr[0];
 
-        if (firstNum < secondNum || secondNum === undefined) {
-            sortedArr.push(firstArr.shift());
-        } else if (secondNum < firstNum || firstNum === undefined) {
-            sortedArr.push(secondArr.shift());
-        }
+    if (firstNum < secondNum || secondNum === undefined) {
+      sortedArr.push(firstArr.shift());
+    } else if (secondNum < firstNum || firstNum === undefined) {
+      sortedArr.push(secondArr.shift());
+    }
+  }
 
-    } 
-
-    return sortedArr;
+  return sortedArr;
 }
 
 function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  } else {
     let splitted = split(array);
     let [firstHalf, secondHalf] = splitted;
-
-    let result = merge(firstHalf, secondHalf);
-
-
-    return result;
-
-
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,31 +1,43 @@
 describe('Split Array function', function() {
-    it('is able to split an array into two halves', function() {
-      expect(split([1,2,3,4])).toEqual([[1,2], [3, 4]]);
-    });
+  it('is able to split an array into two halves', function() {
+    expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
+  });
 
-    it('is able to split odd number of elements', function() {
-        expect(split([1,2,3,4,5])).toEqual([[1,2,3], [4,5]]);
-    });
+  it('is able to split odd number of elements', function() {
+    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2, 3], [4, 5]]);
+  });
 });
 
-describe('Merge function', function(){
-    it('is able to merge two sorted arrays into one sorted array', function(){
-      expect(merge([2], [1])).toEqual([1,2]);
-    });
-    it('is able to merge longer sorted arrays into one sorted array', function() {
-        expect(merge([2,3,4], [1,5,6])).toEqual([1,2,3,4,5,6]);
-    });
+describe('Merge function', function() {
+  it('is able to merge two sorted arrays into one sorted array', function() {
+    expect(merge([2], [1])).toEqual([1, 2]);
+  });
+  it('is able to merge longer sorted arrays into one sorted array', function() {
+    expect(merge([2, 3, 4], [1, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
 
-describe('MergeSort function', function(){
-   it('sorts and merges two element array', function() {
-       expect(mergeSort([2, 1])).toEqual([1,2]);
-   });
-   it('sorts and merges four element array', function() {
-    expect(mergeSort([2,4,1,5])).toEqual([1,2,4,5])
-   ;});
-
-   it('sorts and merges five element array', function() {
-       expect(mergeSort([2,4,1,3,5])).toEqual([1,2,3,4,5]);
-    });
+describe('MergeSort function', function() {
+  it('sorts and merges two element array', function() {
+    expect(mergeSort([2, 1])).toEqual([1, 2]);
+  });
+  it('sorts and merges four element array', function() {
+    expect(mergeSort([2, 4, 1, 5])).toEqual([1, 2, 4, 5]);
+  });
+  it('sorts and merges five element array', function() {
+    expect(mergeSort([2, 4, 1, 3, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+  it('sorts and merges a complicated array', function() {
+    expect(mergeSort([9, 3, 4, 2, 1, 5, 8, 7, 6])).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+    ]);
+  });
 });
